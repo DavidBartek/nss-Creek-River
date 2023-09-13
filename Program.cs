a7b5d3e2087f296a7f73cc1dd0c9d22755671452
DavidBartek <davidwbartek@gmail.com>
2023-09-13 15:46:16 -0500

    all api endpoints and classes built, functional

--- Program.cs
+++ Program.cs
@@ -33,28 +33,141 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+// get campsites collection
+app.MapGet("/api/campsites", (CreekRiverDbContext db) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    return db.Campsites.ToList();
+});
 
-app.MapGet("/weatherforecast", () =>
+// get campsite by specified ID - includes related data (.Include)
+app.MapGet("/api/campsites/{id}", (CreekRiverDbContext db, int id) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    try
+    {
+        Campsite campsite = db.Campsites.Include(c => c.CampsiteType).Single(c => c.Id == id);
+        return Results.Ok(campsite);
+    }
+    catch (InvalidOperationException)
+    {
+        return Results.NotFound();
+    }
+    // alternate method to try/catch: use .SingleOrDefault instead of .Single, handle error in if/else block
+});
+
+// create campsite
+app.MapPost("/api/campsites", (CreekRiverDbContext db, Campsite campsite) =>
+{
+    db.Campsites.Add(campsite);
+    db.SaveChanges();
+    return Results.Created($"/api/campsites/{campsite.Id}", campsite);
+});
+
+// delete campsite by id
+app.MapDelete("/api/campsites/{id}", (CreekRiverDbContext db, int id) =>
+{
+    Campsite campsite = db.Campsites.SingleOrDefault(campsite => campsite.Id == id);
+    if (campsite == null)
+    {
+        return Results.NotFound();
+    }
+    db.Campsites.Remove(campsite);
+    db.SaveChanges();
+    return Results.NoContent();
+
+    // note: by default, EF Core will configure related reservation rows (containing related data to campsites) to be deleted as well.
+    // ^ "cascade delete"
+
+});
+
+// update campsite by id
+app.MapPut("/api/campsites/{id}", (CreekRiverDbContext db, int id, Campsite campsite) =>
+{
+    Campsite campsiteToUpdate = db.Campsites.SingleOrDefault(campsite => campsite.Id == id);
+    if (campsiteToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    campsiteToUpdate.Nickname = campsite.Nickname;
+    campsiteToUpdate.CampsiteTypeId = campsite.CampsiteTypeId;
+    campsiteToUpdate.ImageUrl = campsite.ImageUrl;
+
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+// get reservations - includes related data (.Include, .ThenInclude, .OrderBy)
+app.MapGet("/api/reservations", (CreekRiverDbContext db) =>
+{
+    return db.Reservations
+        .Include(r => r.UserProfile) // JOIN the UserProfiles table
+        .Include(r => r.Campsite) // JOIN the Campsites table
+        .ThenInclude(c => c.CampsiteType) // CampsiteType only accessible through Campsite; .ThenInclude JOINs through Campsite
+        .OrderBy(res => res.CheckinDate) // .OrderBy: regular Linq method, but corresponds to SQL's ORDER BY
+        .ToList();
+});
+
+// ^^ equivalent SQL query:
+// SELECT r.Id,
+//        r.CampsiteId,
+//        r.UserProfileId,
+//        r.CheckinDate,
+//        r.CheckoutDate,
+//        up.Id,
+//        up.FirstName,
+//        up.LastName,
+//        up.Email,
+//        c.Id, 
+//        c.Nickname
+//        c.ImageUrl,
+//        c.CampsiteTypeId, 
+//        ct.Id,
+//        ct.CampsiteTypeName,
+//        ct.MaxReservationDays,
+//        ct.FeePerNight
+// FROM Reservations r
+// LEFT JOIN UserProfiles up ON up.Id = r.UserProfileId
+// LEFT JOIN Campsites c ON c.Id = r.CampsiteId
+// LEFT JOIN CampsiteTypes ct ON ct.Id = c.CampsiteTypeId
+// ORDER BY r.CheckinDate;
+
+// post new reservation
+// error handling example here
+app.MapPost("/api/reservations", (CreekRiverDbContext db, Reservation newRes) =>
+{
+    // Check if reservation checkout is before or the same day as checkin
+    if (newRes.CheckoutDate <= newRes.CheckinDate)
+    {
+        return Results.BadRequest("Reservation checkout must be at least one day after checkin");
+    }
+
+    try
+    {
+        db.Reservations.Add(newRes);
+        db.SaveChanges();
+        return Results.Created($"/api/reservations/{newRes.Id}", newRes);
+    }
+    catch (DbUpdateException)
+    {
+        return Results.BadRequest("Invalid data submitted");
+    }
+    
+});
+
+// delete reservation by id
+app.MapDelete("/api/reservations/{id}", (CreekRiverDbContext db, int id) =>
+{
+    Reservation reservation = db.Reservations.SingleOrDefault(r => r.Id == id);
+    if (reservation == null)
+    {
+        return Results.NotFound();
+    }
+    db.Reservations.Remove(reservation);
+    db.SaveChanges();
+    return Results.NoContent();
+
+    // note: by default, EF Core will configure related reservation rows (containing related data to campsites) to be deleted as well.
+    // ^ "cascade delete"
+
+});
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
